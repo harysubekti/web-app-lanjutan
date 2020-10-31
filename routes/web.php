2e4a67decc4d6fd6fec24ebfcca2fdd8cb8708f9
Hary Subekti <harysubekti20@gmail.com>
2020-10-31 16:27:07 +0800

    melakukan perubahan di file web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerPertama;
+use App\Http\Controllers\ControllerKedua;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Route Untuk Controller
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/view1controll1", [ControllerPertama::class, "first"]);
+Route::get("/view2controll1", [ControllerPertama::class, "second"]);
+Route::get("/view3controll1", [ControllerPertama::class, "third"]);
+Route::get("/view1controll2", [ControllerKedua::class, "pertama"]);
+Route::get("/view2controll2", [ControllerKedua::class, "kedua"]);
+Route::get("/view3controll2", [ControllerKedua::class, "ketiga"]);
+
+//URL Untuk 5 Closure
+Route::get("/biru", function() {
+	return "<h1 style='color:blue;'> Closure</h1>";
+});
+
+Route::get("/hijau", function() {
+	return "<h1 style='color:green;'> Closure</h1>";
+});
+
+Route::get("/merah", function() {
+	return "<h1 style='color:red;'> Closure</h1>";
+});
+
+Route::get("/coklat", function() {
+	return "<h1 style='color:brown;'> Closure</h1>";
+});
+
+Route::get("/pink", function() {
+	return "<h1 style='color:pink;'> Closure</h1>";
 });
